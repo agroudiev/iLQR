05c989e888c32ea3a449a0c628fc06847413f722
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2024-12-23 17:18:34 -0500

    Started the Rust code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 use pyo3::prelude::*;
 
+mod ilqr_solver;
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn test(a: usize, f: Bound<'_, PyAny>) -> PyResult<usize> {
+    f.call1((a,))?.extract()
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn ilqr(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(test, m)?)?;
     Ok(())
 }
