58c1df530c3fb144ba4a806b3bc709fc2de96a90
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2024-12-23 20:13:31 -0500

    Implemented `solve`

--- src/ilqr_solver.rs
+++ src/ilqr_solver.rs
@@ -40,7 +40,7 @@ impl ILQRSolver {
         }
     }
 
-    /// Compute the Jacobians of the dynamics with respect to the state and control,
+    /// Computes the Jacobians of the dynamics with respect to the state and control,
     /// at state `x` and control `u`
     ///
     /// * `x` - The current state
@@ -51,40 +51,87 @@ impl ILQRSolver {
         unimplemented!()
     }
 
-    /// Compute the forward pass from a given state `x` and controls `us`
-    ///
-    /// * `x` - The current state
-    /// * `us` - The control sequence
-    ///
-    /// Returns: a tuple containing the states and loss
-    fn forward(&self, x: DVector<f64>, us: Vec<DVector<f64>>) -> (Vec<DMatrix<f64>>, f64) {
-        unimplemented!()
-    }
-
-    /// Compute the backward pass from a given state `x`, controls `us`, and a target `target`
+    /// Computes the forward pass from a given state `x`, controls `us`, and a `target`
     ///
     /// * `x` - The current state
     /// * `us` - The control sequence
     /// * `target` - The target state
     ///
-    /// Returns: a tuple containing the control gains and the forcing gains
+    /// Returns: a tuple `(xs, loss)` containing the states and loss
+    fn forward(
+        &self,
+        x: &DVector<f64>,
+        us: &Vec<DVector<f64>>,
+        target: &DVector<f64>,
+    ) -> (Vec<DVector<f64>>, f64) {
+        unimplemented!()
+    }
+
+    /// Computes the backward pass from a given state `x`, controls `us`, and a `target`
+    ///
+    /// * `x` - The current state
+    /// * `us` - The control sequence
+    /// * `target` - The target state
+    ///
+    /// Returns: a tuple `(Ks, ds)` containing the control gains and the forcing gains
     fn backward(
         &self,
-        x: DVector<f64>,
-        us: Vec<DVector<f64>>,
-        target: DVector<f64>,
+        xs: &Vec<DVector<f64>>,
+        us: &Vec<DVector<f64>>,
+        target: &DVector<f64>,
     ) -> (Vec<DMatrix<f64>>, Vec<DMatrix<f64>>) {
         unimplemented!()
     }
 
-    /// Solve the ILQR problem from a given initial state `x0` and target `target`
-    /// 
+    /// Solves the ILQR problem from a given initial state `x0` and target `target`
+    ///
     /// * `x0` - The initial state
     /// * `target` - The target state
     /// * `dynamics` - The dynamics function, that computes the next state given the current state `x` and control `u`
-    /// 
+    ///
     /// Returns: the sequence of controls
-    pub fn solve(&self, x0: DVector<f64>, target: DVector<f64>, dynamics: impl Fn(&[f64], &[f64]) -> Vec<f64>) -> Vec<DVector<f64>> {
-        unimplemented!()
+    pub fn solve(
+        &self,
+        x0: DVector<f64>,
+        target: DVector<f64>,
+        dynamics: impl Fn(&[f64], &[f64]) -> DVector<f64>,
+        time_steps: usize,
+        max_iterations: usize,
+        convergence_threshold: f64,
+    ) -> Vec<DVector<f64>> {
+        // Initialize the trajectory
+        let mut us = vec![DVector::zeros(self.control_dim); time_steps];
+        // TODO: implement different initialization strategies
+
+        for _ in 0..max_iterations {
+            // Forward pass
+            let (mut xs, _loss) = self.forward(&x0, &us, &target);
+            // Backward pass
+            #[allow(non_snake_case)]
+            let (Ks, ds) = self.backward(&xs, &us, &target);
+
+            // Update the controls
+            let mut x = x0.clone();
+            let mut dus: Vec<DVector<f64>> = vec![DVector::zeros(self.control_dim); time_steps];
+            for i in 0..time_steps {
+                let du = &Ks[i] * (&x - &xs[i]) + &ds[i];
+
+                // TODO: gradient clip `du`
+
+                us[i] += &du;
+                dus[i] = du;
+
+                x = dynamics(x.as_slice(), us[i].as_slice());
+                xs[i] = x.clone();
+            }
+
+            // Check for convergence
+            let norm = dus.iter().map(|du| du.norm()).sum::<f64>().sqrt();
+            if norm < convergence_threshold {
+                break;
+            }
+        }
+
+        us
     }
 }
--- src/py_ilqr_solver.rs
+++ src/py_ilqr_solver.rs
@@ -1,7 +1,7 @@
 use crate::ilqr_solver::ILQRSolver;
 use nalgebra::{DMatrix, DVector};
-use pyo3::{pyclass, pymethods};
 use pyo3::prelude::*;
+use pyo3::{pyclass, pymethods};
 
 #[pyclass(name = "ILQRSolver")]
 #[derive(Debug)]
@@ -10,16 +10,13 @@ pub struct PyILQRSolver {
     solver: ILQRSolver,
 }
 
+// TODO: replace the `Vec` by numpy arrays
+
 #[pymethods]
 #[allow(non_snake_case)]
 impl PyILQRSolver {
     #[new]
-    fn new(
-        state_dim: usize,
-        control_dim: usize,
-        Q: Vec<Vec<f64>>,
-        R: Vec<Vec<f64>>,
-    ) -> Self {
+    fn new(state_dim: usize, control_dim: usize, Q: Vec<Vec<f64>>, R: Vec<Vec<f64>>) -> Self {
         Self {
             solver: ILQRSolver::new(
                 state_dim,
@@ -30,19 +27,45 @@ impl PyILQRSolver {
         }
     }
 
-    fn solve(&self, x0: Vec<f64>, target: Vec<f64>, dynamics: Bound<'_, PyAny>) -> Vec<Vec<f64>> {
+    #[pyo3(signature = (x0, target, dynamics, time_steps, max_iterations=None, convergence_threshold=None))]
+    fn solve(
+        &self,
+        x0: Vec<f64>,
+        target: Vec<f64>,
+        dynamics: Bound<'_, PyAny>,
+        time_steps: usize,
+        max_iterations: Option<usize>,
+        convergence_threshold: Option<f64>,
+    ) -> Vec<Vec<f64>> {
         // Check the input dimensions
         assert!(x0.len() == self.solver.state_dim, "Invalid state dimension");
-        assert!(target.len() == self.solver.state_dim, "Invalid target dimension");
+        assert!(
+            target.len() == self.solver.state_dim,
+            "Invalid target dimension"
+        );
+        let max_iterations = max_iterations.unwrap_or(100);
+        let convergence_threshold = convergence_threshold.unwrap_or(1e-2);
 
         // Convert the input to nalgebra types
         let x0 = DVector::from_row_slice(&x0);
         let target = DVector::from_row_slice(&target);
 
         // Solve the problem and convert the output
-        let us = self.solver.solve(x0, target, |x, u| {
-            dynamics.call1((x, u)).unwrap().extract().unwrap()
-        });
+        let us = self.solver.solve(
+            x0,
+            target,
+            |x, u| {
+                dynamics
+                    .call1((x, u))
+                    .unwrap()
+                    .extract::<Vec<f64>>()
+                    .unwrap()
+                    .into()
+            },
+            time_steps,
+            max_iterations,
+            convergence_threshold,
+        );
         us.into_iter().map(|x| x.as_slice().to_vec()).collect()
     }
 
